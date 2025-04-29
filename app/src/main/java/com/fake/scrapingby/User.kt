9c2e951d6548d4350c869e9a356323b46544c9d3
Spencer Peters <st10357026@vcconnect.edu.za>
2025-04-29 22:44:28 +0200

    Created Register Page

--- app/src/main/java/com/fake/scrapingby/User.kt
+++ app/src/main/java/com/fake/scrapingby/User.kt
@@ -2,6 +2,7 @@ package com.fake.scrapingby
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.Date
 
 //User table
 @Entity(tableName = "Users")
@@ -9,5 +10,7 @@ data class User(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val username : String,
     val password : String,
-    val profileImage : String? = null //So that the user can add Image later.
+    val profileImage : String? = null, //So that the user can add Image later.
+    val firstName : String,
+    val surname : String
 )
