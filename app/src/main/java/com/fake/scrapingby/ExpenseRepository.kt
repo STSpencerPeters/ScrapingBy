984b70d92afbb66d9fad564cf0f6ded6b27d400d
Spencer Peters <st10357026@vcconnect.edu.za>
2025-05-02 00:20:40 +0200

    Updated Expense Repository.

--- app/src/main/java/com/fake/scrapingby/ExpenseRepository.kt
+++ app/src/main/java/com/fake/scrapingby/ExpenseRepository.kt
@@ -5,4 +5,14 @@ class ExpenseRepository(private val expenseDAO: ExpensesDAO) {
     suspend fun addExpense(expenses: Expenses){
         expenseDAO.createExpense(expenses)
     }
+
+    //Function to list expenses
+    suspend fun fetchExpenses(userId: Int): List<Expenses>{
+        return expenseDAO.showAllExpensesForUser(userId)
+    }
+
+    //Function to list fetch expenses in a category
+    suspend fun fetchExpensesByCategory(userId: Int, categoryId: Int): List<Expenses> {
+        return expenseDAO.getExpensesByCategory(userId, categoryId)
+    }
 }
